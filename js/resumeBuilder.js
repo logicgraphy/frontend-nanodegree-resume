0435c4c76c703944e1396afc39f007d1b8406f02
Randeep Singh <randeepsingh85@gmail.com>
2016-02-22 13:17:49 -0800

    fix: encapsulated section data within display functions

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,156 +4,117 @@ This is empty on purpose! Your code to build the resume will go here.
 
 var bio, work, projects, education;
 
-function loadBioSection(){
-	$.getJSON("./data/bio.json", function(data) {
-		bio = data;
-	    $("#header").prepend(HTMLheaderRole.replace('%data%', data.role));
-	    $("#header").prepend(HTMLheaderName.replace('%data%', data.name));	
+$.getJSON("./data/bio.json", function(data) {
+	bio = data;
+	bio.display = function(){	
+	    $("#header").prepend(HTMLheaderRole.replace('%data%', bio.role));
+	    $("#header").prepend(HTMLheaderName.replace('%data%', bio.name));	
 		
 		var formattedContactGeneric = HTMLcontactGeneric;
 		formattedContactGeneric = formattedContactGeneric.replace('%contact%', "Generic Contact");
 		formattedContactGeneric = formattedContactGeneric.replace('%data%', "To be updated");
 		//$("#topContacts").append(formattedContactGeneric);	
-		$("#topContacts").append(HTMLlocation.replace('%data%',data.contacts.location));
-		$("#topContacts").append(HTMLmobile.replace('%data%',data.contacts.mobile));
-		$("#topContacts").append(HTMLemail.replace('%data%',data.contacts.email));
-		$("#topContacts").append(HTMLtwitter.replace('%data%',data.contacts.twitter));
-		$("#topContacts").append(HTMLgithub.replace('%data%',data.contacts.github));	
-		//$("#topContacts").append(HTMLblog.replace('%data%',data.contacts.blog));
+		$("#topContacts").append(HTMLlocation.replace('%data%',bio.contacts.location));
+		$("#topContacts").append(HTMLmobile.replace('%data%',bio.contacts.mobile));
+		$("#topContacts").append(HTMLemail.replace('%data%',bio.contacts.email));
+		$("#topContacts").append(HTMLtwitter.replace('%data%',bio.contacts.twitter));
+		$("#topContacts").append(HTMLgithub.replace('%data%',bio.contacts.github));	
+		//$("#topContacts").append(HTMLblog.replace('%data%',bio.contacts.blog));
 
-	    $("#header").append(HTMLbioPic.replace('%data%',data.biopic));
-		$("#header").append(HTMLwelcomeMsg.replace('%data%',data.welcomeMessage));
+	    $("#header").append(HTMLbioPic.replace('%data%',bio.biopic));
+		$("#header").append(HTMLwelcomeMsg.replace('%data%',bio.welcomeMessage));
 		$("#header").append(HTMLskillsStart);
-		data.skills.forEach(function(d){
+		bio.skills.forEach(function(d){
 			$("#header").append(HTMLskills.replace('%data%',d));
 		});
-		
-		if($('.flex-item').length === 0) {
-	      $('#topContacts').hide();
-	    }
-	    if($('h1').length === 0) {
-	      $('#header').hide();
-	    }
 
-	});
-}
+		$("#footerContacts").append(HTMLmobile.replace('%data%',bio.contacts.mobile));
+		$("#footerContacts").append(HTMLemail.replace('%data%',bio.contacts.email));		
+		$("#footerContacts").append(HTMLtwitter.replace('%data%',bio.contacts.twitter));		
+	 }
 
-function loadWorkSection(){
-	$.getJSON("./data/work.json", function(data){
-		work = data;	
-		data = data.jobs;		
-		$('#workExperience').append(HTMLworkStart);			
+	bio.display();
+	if($('.flex-item').length === 0) {
+      $('#topContacts').hide();
+    }
+    if($('h1').length === 0) {
+      $('#header').hide();
+	}
+	if($('.flex-item').length === 0) {
+			$('#lets-connect').hide();
+	}
+});
 
-		for(var i in data){	
-			$('.work-entry').append(HTMLworkEmployer.replace('%data%',data[i].employer) + HTMLworkTitle.replace('%data%',data[i].title));
-			$('.work-entry').append(HTMLworkDates.replace('%data%',data[i].dates));
-			$('.work-entry').append(HTMLworkLocation.replace('%data%',data[i].location));
-			$('.work-entry').append(HTMLworkDescription.replace('%data%',data[i].description));
-		}		
+$.getJSON("./data/work.json", function(data){
+	work = data;	
+	work.display = function(){			
+		$('#workExperience').append(HTMLworkStart);			   
+		work.jobs.forEach(function(d){
+			$('.work-entry').append(HTMLworkEmployer.replace('%data%',d.employer) + HTMLworkTitle.replace('%data%',d.title));
+			$('.work-entry').append(HTMLworkDates.replace('%data%',d.dates));
+			$('.work-entry').append(HTMLworkLocation.replace('%data%',d.location));
+			$('.work-entry').append(HTMLworkDescription.replace('%data%',d.description));
+		});		
+	}
+	work.display();
+	if($('.work-entry').length === 0) {
+      	$('#workExperience').hide();
+    }	
+});
 
-		if($('.work-entry').length === 0) {
-	      	$('#workExperience').hide();
-	    }
-	});
-}
-
-function loadProjectsSection(){
-	$.getJSON("./data/projects.json", function(data){
-		projects = data;
-		data = data.projects;
-		$('#projects').append(HTMLprojectStart);
-		
-		for(var i in data){		
-			$('.project-entry').append(HTMLprojectTitle.replace('%data%', data[i].title));
-			$('.project-entry').append(HTMLprojectDates.replace('%data%', data[i].dates));
-			$('.project-entry').append(HTMLprojectDescription.replace('%data%', data[i].description));
-			data[i].images.forEach(function(d){
-				$('.project-entry').append(HTMLprojectImage.replace('%data%', d));
+$.getJSON("./data/projects.json", function(data){
+	projects = data;
+	projects.display = function(){					
+		$('#projects').append(HTMLprojectStart);		
+		projects.projects.forEach(function(d){
+			$('.project-entry').append(HTMLprojectTitle.replace('%data%', d.title));
+			$('.project-entry').append(HTMLprojectDates.replace('%data%', d.dates));
+			$('.project-entry').append(HTMLprojectDescription.replace('%data%', d.description));
+			d.images.forEach(function(i){
+				$('.project-entry').append(HTMLprojectImage.replace('%data%', i));
 			});
-			
-		}
-		if($('.project-entry').length === 0) {
-	      	$('#projects').hide();
-	    }	
-	});
-}
+		});
+	}
+	projects.display();
+	if($('.project-entry').length === 0) {
+      	$('#projects').hide();
+    }		
+});
 
-function loadEducationSection(){
-	$.getJSON("./data/education.json", function(data){
-		education = data;
-		var schools  = data.schools;
-		var onlineCourses = data.onlineCourses;
+
+$.getJSON("./data/education.json", function(data){
+	education = data;
+	education.display = function(){
+		var schools  = education.schools;
+		var onlineCourses = education.onlineCourses;
 		
 		$('#education').append(HTMLschoolStart);
-		for(var i in schools){
-			$('.education-entry').append(HTMLschoolName.replace('#', schools[i].url).replace('%data%', schools[i].name) + HTMLschoolDegree.replace('%data%', schools[i].degree));		
-			$('.education-entry').append(HTMLschoolDates.replace('%data%', schools[i].dates));
-			$('.education-entry').append(HTMLschoolLocation.replace('%data%', schools[i].location));
-			schools[i].majors.forEach(function(d){
-				$('.education-entry').append(HTMLschoolMajor.replace('%data%', d));
+		schools.forEach(function(d){
+			$('.education-entry').append(HTMLschoolName.replace('#', d.url).replace('%data%', d.name) + HTMLschoolDegree.replace('%data%', d.degree));		
+			$('.education-entry').append(HTMLschoolDates.replace('%data%', d.dates));
+			$('.education-entry').append(HTMLschoolLocation.replace('%data%', d.location));
+			d.majors.forEach(function(m){
+				$('.education-entry').append(HTMLschoolMajor.replace('%data%', m));
 			});
-			
-		}
+		});
 		$('.education-entry').append(HTMLonlineClasses);	
-		for(var j in onlineCourses){
-			
-			$('.education-entry').append(HTMLonlineTitle.replace('%data%', onlineCourses[j].title) + HTMLonlineSchool.replace('%data%',onlineCourses[j].school));
-			$('.education-entry').append(HTMLonlineDates.replace('%data%',onlineCourses[j].date));
-			$('.education-entry').append(HTMLonlineURL.replace('%data%',onlineCourses[j].url));
-		}
-
-		if($('.education-entry').length === 0) {
-	      	$('#education').hide();
-	    }
-	});
-}
+		onlineCourses.forEach(function(d){
+			$('.education-entry').append(HTMLonlineTitle.replace('%data%', d.title) + HTMLonlineSchool.replace('%data%',d.school));
+			$('.education-entry').append(HTMLonlineDates.replace('%data%',d.date));
+			$('.education-entry').append(HTMLonlineURL.replace('%data%',d.url));
+		});
+	}
+	education.display();
+	if($('.education-entry').length === 0) {
+      	$('#education').hide();
+    }	
+});
 
 function loadMapSection(){
 	$('#mapDiv').append(googleMap);
 	if($('#map') === null) {
 	    $('#mapDiv').hide();
 	}	
-}
+}  
 
-function loadLetsConnectionSection(){
-	$.getJSON("./data/bio.json", function(data) {
-		$("#footerContacts").append(HTMLmobile.replace('%data%',data.contacts.mobile));
-		$("#footerContacts").append(HTMLemail.replace('%data%',data.contacts.email));		
-		$("#footerContacts").append(HTMLtwitter.replace('%data%',data.contacts.twitter));
-		
-		if($('.flex-item').length === 0) {
-			$('#lets-connect').hide();
-		}
-	});
-}    
-
-//Call methods to load data 
-loadBioSection();
-loadWorkSection();
-loadEducationSection();
-loadProjectsSection();
 loadMapSection();
-loadLetsConnectionSection();
-    
-
-
-
-
-
-
-	
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
